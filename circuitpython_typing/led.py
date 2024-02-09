f15a7156327640917878c423134d7f4c43486685
Justin Myers <justmobilize@users.noreply.github.com>
2024-02-09 09:07:47 -0800

    Add missing def sort

--- circuitpython_typing/led.py
+++ circuitpython_typing/led.py
@@ -11,9 +11,9 @@ Type annotation definitions for LEDs.
 * Author(s): Alec Delaney
 """
 
-# Protocol was introduced in Python 3.8, TypeAlias in 3.10
 from typing import Tuple, Union
 
+# Protocol was introduced in Python 3.8, TypeAlias in 3.10
 from typing_extensions import Protocol, TypeAlias
 
 ColorBasedColorUnion: TypeAlias = Union[int, Tuple[int, int, int]]
